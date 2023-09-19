6f18017ea482adc3ada926a94759df587fb3ec8c
sanzhh <sanzhar.maratov@proton.me>
2023-09-19 17:50:59 +0600

    [changed] added base logger context to main, initialized db clients

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/ppaprikaa/shorty/internal/app"
 	"github.com/ppaprikaa/shorty/internal/config"
+	"github.com/ppaprikaa/shorty/internal/db/mongo"
+	"github.com/ppaprikaa/shorty/internal/db/redis"
 	"github.com/ppaprikaa/shorty/internal/log"
 	"go.uber.org/fx"
 )
@@ -11,6 +13,11 @@ func main() {
 	fx.New(
 		config.Module,
 		log.Module,
+		// Base log context
+		// Couldn't initialize, because it is not obvious that module returns base context
+		fx.Provide(log.Context),
+		mongo.Module,
+		redis.Module,
 		app.Module,
 	).Run()
 }
