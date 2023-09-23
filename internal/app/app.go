56623c26ddb63aba15932c079810f3fb379bf228
sanzhh <sanzhar.maratov@proton.me>
2023-09-23 22:26:10 +0600

    [changed] got rid of the app struct

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,59 +1,15 @@
 package app
 
 import (
-	"context"
-
-	"github.com/ppaprikaa/shorty/internal/adapters/routers/chi"
-	"github.com/ppaprikaa/shorty/internal/adapters/servers/http"
-	"github.com/ppaprikaa/shorty/internal/core/ports"
-	"github.com/ppaprikaa/shorty/internal/log"
+	httphandlers "github.com/ppaprikaa/shorty/internal/adapters/handlers/http"
+	chirouter "github.com/ppaprikaa/shorty/internal/adapters/routers/chi"
+	httpserver "github.com/ppaprikaa/shorty/internal/adapters/servers/http"
 	"go.uber.org/fx"
-	"golang.org/x/exp/slog"
 )
 
 var Module = fx.Module(
 	"app",
-	http.Module,
-	chi.Module,
-	fx.Provide(New),
-	fx.Invoke(func(*app) {}),
+	httpserver.Module,
+	httphandlers.Module,
+	chirouter.Module,
 )
-
-type app struct {
-	server ports.Server
-	log    *slog.Logger
-}
-
-func New(lc fx.Lifecycle, server ports.Server, logger *slog.Logger) *app {
-	var app = &app{
-		server: server,
-		log:    logger,
-	}
-
-	lc.Append(
-		fx.Hook{
-			OnStart: func(ctx context.Context) error { return app.Run(ctx) },
-			OnStop:  func(ctx context.Context) error { return app.Stop(ctx) },
-		},
-	)
-
-	return app
-}
-
-func (a *app) Run(ctx context.Context) error {
-	a.log.Info("Starting application")
-	if err := a.server.Run(log.WithContext(ctx, a.log)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (a *app) Stop(ctx context.Context) error {
-	a.log.Info("Stopping application")
-	if err := a.server.Shutdown(ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
