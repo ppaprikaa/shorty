3c5a1283bd68421431c1fdb2b6c5f2dd7d3aa300
sanzhh <sanzhar.maratov@proton.me>
2023-09-23 22:29:50 +0600

    [changed] chi router constructor function
    added args struct to increase code readability and usability by consolidating all handlers in one structure

--- internal/adapters/routers/chi/chi.go
+++ internal/adapters/routers/chi/chi.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/ppaprikaa/shorty/internal/adapters/handlers/http/server"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Module(
 	"routers.chi",
 	fx.Provide(
+		NewHandlers,
 		fx.Annotate(
 			New,
 			fx.As(new(http.Handler)),
@@ -17,8 +19,20 @@ var Module = fx.Module(
 	),
 )
 
-func New() *chi.Mux {
+func NewHandlers(server *server.Handler) *handlers {
+	handlers := new(handlers)
+	handlers.Server = server
+	return handlers
+}
+
+type handlers struct {
+	Server *server.Handler
+}
+
+func New(handlers *handlers) *chi.Mux {
 	mux := chi.NewMux()
+
+	mux.Get("/api/v1/healthcheck", handlers.Server.Healthcheck())
 
 	return mux
 }
